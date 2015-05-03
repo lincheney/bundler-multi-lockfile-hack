0ae5f5abb4e3dc243255c63e1a30e37b762c2c00
Cheney Lin <lincheney@gmail.com>
2015-05-03 18:52:44 +1000

    first pass at hack working on bundler v1.5.3

--- bundler/multi_lockfile_hack.rb
+++ bundler/multi_lockfile_hack.rb
@@ -1,2 +1,45 @@
 module Bundler::MultiLockfileHack
+
+  Locks = Struct.new(:gemfile, :groups, :lockfile)
+
+  def self.lockfiles; @lockfiles ||= {}; end
+
+  def to_definition(lockfile, unlock)
+    super
+    DefinitionHack.new(lockfile, @dependencies, @sources, unlock, @ruby_version)
+  end
+
+  def generate_lockfile(lockfile: nil, gemfile: nil, groups: nil)
+    raise 'Expected one of gemfile or groups' unless gemfile or groups
+
+    groups &&= Array(groups)
+    lockfile ||= [gemfile, groups, 'lock'].flatten.compact.join('.')
+    Bundler::MultiLockfileHack.lockfiles[lockfile] = Locks.new(gemfile, groups, lockfile)
+  end
+
+  class DefinitionHack < Bundler::Definition
+    def lock(*)
+      result = super
+      Bundler::MultiLockfileHack.lockfiles.values.each{|data| _write_lock(data)}
+      result
+    end
+
+    def _write_lock(data)
+      if data.gemfile
+        deps = Bundler::Dsl.new.eval_gemfile(data.gemfile)
+      else
+        deps = @dependencies
+      end
+
+      deps = deps.select{|d| (d.groups & data.groups).any?} if data.groups
+      DefinitionHack2.new(data.lockfile, deps, @sources, @unlock, @ruby_version).lock(data.lockfile)
+    end
+  end
+
+  class DefinitionHack2 < Bundler::Definition
+    def resolve
+      super.select{|spec| @dependencies.map(&:name).include? spec.name}
+    end
+  end
+
 end
---
+++ spec/test_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+def read_lockfile(path)
+  Bundler::LockfileParser.new(Bundler.read_file(path))
+end
+
+lockfiles = {
+  "doc.lock" => %w{ rdoc sdoc },
+  "doc.test.lock" => %w{ rdoc sdoc rspec rspec-rails shoulda-matchers },
+  "other.lock" => %w{ json activesupport maruku },
+  "rails.test.lock" => %w{ rspec-rails shoulda-matchers },
+}
+
+shared_examples 'a lockfile writer' do
+  lockfiles.each do |file, deps|
+    it "#{file} should have a subset of Gemfile.lock's platforms" do
+      lockfile = read_lockfile(file)
+      expect(gemfile_lock.platforms).to include(*lockfile.platforms)
+    end
+
+    it "#{file} should have a subset of Gemfile.lock's dependencies" do
+      lockfile = read_lockfile(file)
+      expect(gemfile_lock.dependencies).to include(*lockfile.dependencies)
+    end
+
+    it "#{file} should have a subset of Gemfile.lock's specs" do
+      lockfile = read_lockfile(file)
+      expect(gemfile_lock.specs).to include(*lockfile.specs)
+    end
+
+    it "#{file} should have all the correct dependencies" do
+      lockfile = read_lockfile(file)
+      expect(lockfile.dependencies.map(&:name)).to match_array(deps)
+    end
+  end
+end
+
+describe 'multi lockfile hack' do
+  let(:gemfile_lock)  { read_lockfile('Gemfile.lock') }
+
+  describe 'install' do
+
+    context 'with no Gemfile.lock' do
+      before(:all) do
+        Dir.chdir(File.expand_path('install', __dir__))
+        FileUtils.rm(Dir.glob('*.lock'))
+        system "bundle install"
+      end
+
+      it_behaves_like 'a lockfile writer'
+    end
+  end
+end
