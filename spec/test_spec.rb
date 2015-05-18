96e585a2aec0580a1a6d749be5384736084fe508
Cheney Lin <lincheney@gmail.com>
2015-05-18 22:54:14 +1000

    make teh submodule relative now

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require 'securerandom'
 require 'fileutils'
 require 'bundler'
-require 'erb'
 p Bundler
 
 def read_lockfile(path)
@@ -91,10 +90,7 @@ end
 
 describe 'multi lockfile hack' do
   def compare_lockfiles(a, b, equal=true)
-    files = [a, b].map{|file| File.read(file)}
-    files[1] = ERB.new(files[1]).result
-    files.map!{|file| file.strip.split("\n\n")}
-    
+    files = [a, b].map{|file| File.read(file).strip.split("\n\n")}
     if equal
       expect(files[0]).to match_array(files[1])
     else
@@ -102,14 +98,9 @@ describe 'multi lockfile hack' do
     end
   end
 
-  def copy_lockfile
-    system("erb Gemfile.lock.backup > Gemfile.lock")
-  end
-
   let(:gemfile_lock)  { read_lockfile('Gemfile.lock') }
 
   before(:all) do
-    ENV['__PATH__'] = File.dirname(__FILE__)
     Dir.chdir(File.expand_path('test-dir', File.dirname(__FILE__)))
     FileUtils.rm(Dir.glob('*.lock'))
   end
@@ -125,7 +116,7 @@ describe 'multi lockfile hack' do
 
     context 'with an existing Gemfile.lock' do
       before(:all) do
-        copy_lockfile
+        FileUtils.cp('Gemfile.lock.backup', 'Gemfile.lock')
         bundle('install')
       end
 
@@ -160,7 +151,7 @@ describe 'multi lockfile hack' do
 
     context 'with an existing Gemfile.lock' do
       before(:all) do
-        copy_lockfile
+        FileUtils.cp('Gemfile.lock.backup', 'Gemfile.lock')
         bundle('update')
       end
 
